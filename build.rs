a6dc5894a6be30469e870c0f445bd73d3c1d2bfc
Hovsep Papoyan <papoyanhovsep93@gmail.com>
2024-05-14 18:00:34 +0000

    Updating build.rs

--- build.rs
+++ build.rs
@@ -11,11 +11,7 @@ fn main()
         .flag_if_supported("-Wall")
         .flag_if_supported("-Werror")
         .flag_if_supported("-O3")
-        //.flag_if_supported("-DOPENFHE_VERSION=1.1.4")
         .flag_if_supported("-Wno-parentheses") // [-Wparentheses]
-        //.flag_if_supported("-DMATHBACKEND=4")
-        .flag_if_supported("-fopenmp") // [-Wunknown-pragmas]
-        .flag_if_supported("-fPIE")
         .flag_if_supported("-Wno-unused-parameter") // [-Wunused-parameter]
         .flag_if_supported("-Wno-missing-field-initializers") // [-Wmissing-field-initializers]
         .flag_if_supported("-Wno-unused-function") // [-Wunused-function]
@@ -25,15 +21,15 @@ fn main()
     println!("cargo::rerun-if-changed=src/bindings.hpp");
     println!("cargo::rerun-if-changed=src/bindings.cc");
 
-    // linking additional shared libraries
+    // linking openFHE
     println!("cargo::rustc-link-arg=-L/usr/local/lib");
     println!("cargo::rustc-link-arg=-lOPENFHEpke");
     println!("cargo::rustc-link-arg=-lOPENFHEbinfhe");
     println!("cargo::rustc-link-arg=-lOPENFHEcore");
 
-    println!("cargo::rustc-link-arg=-L/usr/lib/gcc/x86_64-linux-gnu/11");
-    println!("cargo::rustc-link-arg=-lgomp");
+    // linking OpenMP
+    println!("cargo::rustc-link-arg=-fopenmp");
     
-    // necessary to avoid using LD_LIBRARY_PATH
+    // necessary to avoid LD_LIBRARY_PATH
     println!("cargo::rustc-link-arg=-Wl,-rpath=/usr/local/lib");
 }
