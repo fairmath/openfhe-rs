0d7cab8948c1464419524d749690643a7d0b6cf2
Hovsep Papoyan <papoyanhovsep93@gmail.com>
2024-06-19 20:22:35 +0000

    Adding publishing action

---
+++ .github/change_version.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+original_version_str="$(cat ${GITHUB_WORKSPACE}/Cargo.toml | grep "version = ")"
+version_number="$(echo ${original_version_str} | cut -d "\"" -f2)"
+
+major="${version_number%%.*}"
+version_number="${version_number#*.}"
+minor="${version_number%%.*}"
+patch="${version_number#*.}"
+
+if [ ${CHANGE_TYPE} == major ]
+then
+  major=$((major+1))
+elif [ ${CHANGE_TYPE} == minor ]
+then
+  minor=$((minor+1))
+else
+  patch=$((patch+1))          
+fi
+
+new_version_str='version = "'${major}.${minor}.${patch}'"'
+sed -i "s|${original_version_str}|${new_version_str}|g" ${GITHUB_WORKSPACE}/Cargo.toml
+
