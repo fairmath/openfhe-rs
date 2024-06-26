1d7d23d89f1fd5e697d4e8933b46235bf420fc89
nkaskov <mail.nikita.kaskov@gmail.com>
2024-06-26 12:04:02 +0000

    Fixed version update script.

--- .github/change_version.sh
+++ .github/change_version.sh
@@ -11,13 +11,20 @@ patch="${version_number#*.}"
 if [[ ${CHANGE_TYPE} == "major" ]]
 then
   major=$((major+1))
+  minor=0
+  patch=0
 elif [[ ${CHANGE_TYPE} == "minor" ]]
 then
   minor=$((minor+1))
+  patch=0
 else
   patch=$((patch+1))          
 fi
 
 new_version_str='version = "'${major}.${minor}.${patch}'"'
+
+echo "Previous version: ${original_version_str}"
+echo "New version: ${new_version_str}"
+
 sed -i "s|${original_version_str}|${new_version_str}|g" ${GITHUB_WORKSPACE}/Cargo.toml
 
